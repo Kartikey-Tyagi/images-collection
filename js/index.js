ef1fc36b6601dfd57b3b57c0f805e61e5be3ebef
Kartikey Tyagi <kartikeytyagi013@gmail.com>
2021-11-10 22:44:06 +0530

    Fixed mini=or loading issues

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@
     const mainElement = document.querySelector("main");
     let myData;
     let myNum = Math.floor(Math.random() * 33);
-    console.log(myNum);
     let count = myNum;
     let url = `https://picsum.photos/v2/list?page=${count}&limit=30`;
+    mainElement.innerHTML = `<div class="loader"><img src="./img/loader.gif"></div>`;
 
     // Fetch images api
     async function getData(url) {
@@ -17,18 +17,20 @@
         }
     };
     getData(url);
+
     // adding images card
-    function addCards(data) {
+    async function addCards(data) {
         const card = document.createElement("article");
-        card.innerHTML = `<div class='container'>${createCardContent(data)}</div>`;
+        card.innerHTML = `<div class='container'>${await createCardContent(data)}</div>`;
         mainElement.appendChild(card);
+        hideLoader();
     };
     // creating images card content
-    function createCardContent(data) {
-        let html = `<div class='imgBox'><a href='${data.download_url}' target='_blank'><img src='${data.download_url}' alt='background_image'></a></div>`;
-        html += `<div class='content'><h3>Credits: ${data.author}</h3>`;
-        html += `<p><strong>Size:</strong> ${data.width} x ${data.height}</p>`;
-        html += `<p><strong>Source:</strong> <a href='${data.url}' target='_blank'> ${data.url}</a></p></div>`;
+    async function createCardContent(data) {
+        let html = `<div class='imgBox'><a href='${await data.download_url}' target='_blank'><img src='${await data.download_url}' alt='background_image'></a></div>`;
+        html += `<div class='content'><h3>Credits: ${await data.author}</h3>`;
+        html += `<p><strong>Size:</strong> ${data.width} x ${await data.height}</p>`;
+        html += `<p><strong>Source:</strong> <a href='${await data.url}' target='_blank'> ${data.url}</a></p></div>`;
         return html;
     };
     // Next button
@@ -38,22 +40,23 @@
             count = 0;
         }
         count++;
-        mainElement.innerHTML = '';
+        mainElement.innerHTML = `<div class="loader"><img src="./img/loader.gif"></div>`;
         url = `https://picsum.photos/v2/list?page=${count}&limit=30`;
-        console.log(url);
         getData(url);
 
     });
     // Previous button
-    document.getElementById("previous").addEventListener('click', function (e) {
+    document.getElementById("previous").addEventListener('click',function (e) {
         e.preventDefault();
         if (count === 1) {
             count = 34;
         }
         count--;
-        mainElement.innerHTML = '';
+        mainElement.innerHTML = `<div class="loader"><img src="./img/loader.gif"></div>`;
         url = `https://picsum.photos/v2/list?page=${count}&limit=30`;
-        console.log(url);
         getData(url);
     });
+    function hideLoader(){
+        document.querySelector('.loader').style.display = "none";
+    }
 }());
